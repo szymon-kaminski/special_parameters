bd5b1f222b82b17fa1f146d7057be0cc0543165c
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-05-30 22:31:14 +0200

    Add example of keyword-only parameter using '*'

--- advanced_function_parameters.py
+++ advanced_function_parameters.py
@@ -1,7 +1,14 @@
-# Demonstrates a positional-only parameter using the '/' symbol.
+### Demonstrates a positional-only parameter using the '/' symbol.
 def greet(name, /):
     print(f"Hello {name}!")
 
 greet("Szymon")
 # greet("name=Szymon") # This would raise an error!
 
+
+### Demonstrates a keyword-only parameter using the '*' symbol.
+def show_age(*, age):
+    print(f"You are {age} years old.")
+
+show_age(age=35)
+# show_age(35)  # This would raise an error!
